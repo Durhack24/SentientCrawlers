1786d39f4c1879ae1f060d5fc1ca7e13c24262ff
Matty Muir <mattygmuir@gmail.com>
2024-11-02 17:02:32 +0000

    Add RiverPoint.

--- SentientCrawlers/Simulator/Map.h
+++ SentientCrawlers/Simulator/Map.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <vector>
 
+#include "CollegeBar.h"
+#include "RiverPoint.h"
+
 class Map
 {
 public:
-
+	static const std::vector<CollegeBar>& GetBars();
+	static const std::vector<RiverPoint>& GetRiver();
 };
---
+++ SentientCrawlers/Simulator/RiverPoint.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct RiverPoint
+{
+	int x, y;
+};
